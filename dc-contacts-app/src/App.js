8907a04614577ccfdfeaddbaea092dc37c6695c3
Josh Ladwig <joshua_ladwig@yahoo.com>
2018-06-18 07:23:56 -0500

    updated code with ESLint

--- dc-contacts-app/src/App.js
+++ dc-contacts-app/src/App.js
@@ -1,37 +1,41 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NewContactForm from './NewContactForm';
-import ShowContacts from './ShowContacts';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NewContactForm from './NewContactForm'
+import ShowContacts from './ShowContacts'
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      name: "a_name",
-      email: "an_email",
-      phone: "a_phoneNumber",
-      address: "an_address",
-      city: "a_city",
-      state: "a_state",
-      zip: "a_zip"
+      name: 'a_name',
+      email: 'an_email',
+      phone: 'a_phoneNumber',
+      address: 'an_address',
+      city: 'a_city',
+      state: 'a_state',
+      zip: 'a_zip',
+      contacts: []
     }
-    this.updateContact = this.updateContact.bind(this);
+    this.updateContact = this.updateContact.bind(this)
   }
 
-  updateContact(aContact) {
-    this.setState({
-      name: aContact.name,
-      email: aContact.email,
-      phone: aContact.phone,
-      address: aContact.address,
-      city: aContact.city,
-      state: aContact.state,
-      zip: aContact.zip
-    });
+  updateContact (aContact) {
+    var contacts = [...this.state.contacts, aContact]
+    this.setState({contacts: contacts})
+    // this.setState({
+    //   name: aContact.name,
+    //   email: aContact.email,
+    //   phone: aContact.phone,
+    //   address: aContact.address,
+    //   city: aContact.city,
+    //   state: aContact.state,
+    //   zip: aContact.zip,
+    //   contacts: aContact
+    // });
   }
 
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
@@ -41,11 +45,12 @@ class App extends Component {
         <p className="App-intro">
           Let's start by putting my shit here first!
         </p>
+        {console.log('What is contacts array ', this.state.contacts)}
         <NewContactForm {...this.state} onUpdateContact={this.updateContact} />
         <ShowContacts {...this.state} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- dc-contacts-app/src/NewContactForm.js
+++ dc-contacts-app/src/NewContactForm.js
@@ -1,45 +1,41 @@
-import React from 'react';
-
-
+import React from 'react'
 
 class NewContactForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor (props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    handleSubmit(e){
-        e.preventDefault();
-        const NewContact = {
-            name: e.target.name.value,
-            email: e.target.ename.value,
-            phone: e.target.phoneNumber.value,
-            address: e.target.address.value,
-            city: e.target.city.value,
-            state: e.target.state.value,
-            zip: e.target.zip.value
-        };
-        console.log("new contact is: ", NewContact);
-        this.props.onUpdateContact(NewContact);
-
+  handleSubmit (e) {
+    e.preventDefault()
+    const NewContact = {
+      name: e.target.name.value,
+      email: e.target.ename.value,
+      phone: e.target.phoneNumber.value,
+      address: e.target.address.value,
+      city: e.target.city.value,
+      state: e.target.state.value,
+      zip: e.target.zip.value
     }
-    render() {
-        return (
-            <div>
-            <h2>Enter a new contact here:</h2>
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" name="name" placeholder="Name" /><br />
-                <input type="text" name="ename" placeholder="Email" /><br />
-                <input type="text" name="phoneNumber" placeholder="Phone Number" /><br />
-                <input type="text" name="address" placeholder="Address" /><br />
-                <input type="text" name="city" placeholder="City" /><br />
-                <input type="text" name="state" placeholder="State" /><br />
-                <input type="text" name="zip" placeholder="Zip" /><br />
-                <button type="submit">Submit</button>
-            </form>
-            </div>
-        );
-    }
+    this.props.onUpdateContact(NewContact)
+  }
+  render () {
+    return (
+      <div>
+        <h2>Enter a new contact here:</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name" placeholder="Name" /><br />
+          <input type="text" name="ename" placeholder="Email" /><br />
+          <input type="text" name="phoneNumber" placeholder="Phone Number" /><br />
+          <input type="text" name="address" placeholder="Address" /><br />
+          <input type="text" name="city" placeholder="City" /><br />
+          <input type="text" name="state" placeholder="State" /><br />
+          <input type="text" name="zip" placeholder="Zip" /><br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default NewContactForm;
+export default NewContactForm
